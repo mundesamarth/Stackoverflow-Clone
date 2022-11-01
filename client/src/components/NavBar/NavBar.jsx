18bb0edc3217ee104b07cec5121b44b6b5525896
samarth <munde.samarth28@gmail.com>
2022-11-01 16:58:48 +0530

    navbarStart

---
+++ client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NavBar.css'
+
+function NavBar() {
+  return (
+    <div className='Navbar'>
+      <h1>Hello I am NAVBAR</h1>
+    </div>
+  )
+}
+
+export default NavBar
