9cff80c665dbe3e707a161a30c0aef4a12a5493f
samarth <munde.samarth28@gmail.com>
2022-11-01 19:23:56 +0530

    Nav Header bro

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,12 +1,51 @@
-import React from 'react'
-import './NavBar.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assessts/logo.png";
+import SearchIcon from "@mui/icons-material/Search";
+import Avatar from "../../components/Avatar/Avatar";
+import Button from "../../components/Button/Button";
+import './NavBar.css';
 
 function NavBar() {
+  var User = null;
+
   return (
-    <div className='Navbar'>
-      <h1>Hello I am NAVBAR</h1>
-    </div>
-  )
+    <nav>
+      <div className="Navbar">
+        <Link to="/" className="nav-item nav-btn nav-logo">
+          <img src={logo} alt="logo" />
+        </Link>
+        <Link to="/" className="nav-item nav-btn">
+          About
+        </Link>
+        <Link to="/" className="nav-item nav-btn">
+          Products
+        </Link>
+        <Link to="/" className="nav-item nav-btn">
+          For Teams
+        </Link>
+
+        <form>
+          <input type="text" placeholder="Search..." />
+          <SearchIcon className="nav-search" />
+        </form>
+
+        {User === null ? (
+          <Link to="/auth" className="nav-item nav-links">
+            Log In{" "}
+          </Link>
+        ) : (
+          <>
+            <Link to="/">
+              {" "}
+              <Avatar>S</Avatar>
+            </Link>
+            <Button>Log Out</Button>
+          </>
+        )}
+      </div>
+    </nav>
+  );
 }
 
-export default NavBar
+export default NavBar;
