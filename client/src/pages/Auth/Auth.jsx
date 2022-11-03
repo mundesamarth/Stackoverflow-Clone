483a66ad4486afd92ed232ee7e01180dc37cd5b0
samarth <munde.samarth28@gmail.com>
2022-11-03 22:02:23 +0530

    auth on progress

--- client/src/pages/Auth/Auth.jsx
+++ client/src/pages/Auth/Auth.jsx
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Auth.css";
+import icon from "../../assessts/icon.png";
 
 function Auth() {
+  const [isSignUp, setIsSignUp] = useState(false);
+
+  const handleSwitch = () => {
+    setIsSignUp(!isSignUp);
+  };
   return (
-    <div>
-      <h1>This is Auth</h1>
-      <h1>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-        debitis numquam nam molestiae, magni distinctio, magnam dolorum,
-        pariatur dicta odit omnis corrupti. Modi amet reiciendis rerum pariatur
-        vero sint quas.
-      </h1>
-    </div>
+    <section className="auth-section">
+      <div className="auth-container">
+        {!isSignUp && <img src={icon} alt="StackLogo" className="login-logo" />}
+
+        <form>
+          <label htmlFor="email">
+            <h4>Email</h4>
+            <input type="email" name="email" id="email" />
+          </label>
+          <label htmlFor="password">
+            <div className="password">
+              <h4>Password</h4>
+              {!isSignUp && <h4>forgot password?</h4>}
+            </div>
+            <input type="password" name="password" id="password" />
+          </label>
+          <button type="submit" className="auth-btn">
+            {isSignUp ? "Sign Up" : "Log In"}
+          </button>
+        </form>
+        <p>
+          {isSignUp ? "already have an account ? " : "Dont have an account?"}
+          <button
+            type="button"
+            className="handle-switch-button"
+            onClick={handleSwitch}
+          >
+            {isSignUp ? "Log In" : "sign up"}
+          </button>
+        </p>
+      </div>
+    </section>
   );
 }
 
