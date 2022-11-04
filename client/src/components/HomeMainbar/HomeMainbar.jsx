ca8f421018b242fc2bd20da54b539373be0f9d1b
samarth <munde.samarth28@gmail.com>
2022-11-04 16:09:07 +0530

    navBar start

---
+++ client/src/components/HomeMainbar/HomeMainbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HomeMainbar() {
+  return (
+    <div>HomeMainbar</div>
+  )
+}
+
+export default HomeMainbar
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import LeftSidebar from '../../components/LeftSidebar/LeftSidebar'
+import RightSidebar from '../../components/RightSidebar/RightSidebar'
+import HomeMainbar from '../../components/HomeMainbar/HomeMainbar'
+import "../../App.css"
+
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home-container-1'>
+        <LeftSidebar/>
+        <div className="home-container-2">
+          <HomeMainbar/>
+          <RightSidebar/>
+        </div>
+    </div>
   )
 }
 
